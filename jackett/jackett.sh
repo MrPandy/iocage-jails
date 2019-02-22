a439f5edc40b6f4f402ff38c20c6f98579deabaa
atran <atran@sysaw01.neidinc.local>
2019-02-22 16:34:36 -0500

    more changes

--- jackett/jackett.sh
+++ jackett/jackett.sh
@@ -10,11 +10,9 @@ echo "Enter the default gateway of your network: "
 read gwip
 echo "You entered $gwip"
 sleep 1
-
-iocage create -n "jackett" -r 11.2-RELEASE ip4_addr="vnet0|$jackettip/24" defaultrouter=$gwip vnet="on" allow_raw_sockets="1" boot="on"
-
+#
 echo '{"pkgs":["nano","mono","curl"]}' > /tmp/pkg.json
-iocage create -n "jackett" -p /tmp/pkg.json -r 11.2-RELEASE ip4_addr="vnet0|10.0.0.10/24" defaultrouter="10.0.0.1" vnet="on" allow_raw_sockets="1" boot="on"
-rm /tmp/pkg.json
+iocage create -n "jackett" -p /tmp/pkg.json -r 11.2-RELEASE ip4_addr="vnet0|$jackettip/24" defaultrouter=$gwip vnet="on" allow_raw_sockets="1" boot="on"
+rm -f /tmp/pkg.json
 
 iocage exec jackett "fetch https://github.com/Jackett/Jackett/releases/download/v0.10.805/Jackett.Binaries.Mono.tar.gz -o /usr/local/share"
