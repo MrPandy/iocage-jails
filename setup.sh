655f91e41194de359cc4c2422cb915a77cbc93c1
atran <atran@sysaw01.neidinc.local>
2019-02-22 17:06:07 -0500

    fix paths

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@ echo "Iocage Jails Installation Script in FreeNAS 11.2-U2"
 echo "Which jail you want to install?"
 select ans in "plexpass" "sonarr" "radarr" "jackett"  "transmission" "Quit"; do
     case $ans in
-        plexpass ) /bin/zsh ./plexmediaserver/plexmediaserver_plexpass.sh; break;;
-        sonarr ) /bin/zsh ./sonarr.sh; break;;
-        radarr ) /bin/zsh ./radarr.sh; break;;
-	    jackett ) /bin/zsh ./jackett.sh; break;;
-        transmission ) /bin/zsh ./transmission.sh; break;;
+        plexpass ) /bin/zsh ./plexmediaserver/install-plexpass.sh; break;;
+        sonarr ) /bin/zsh ./install-sonarr.sh; break;;
+        radarr ) /bin/zsh ./install-radarr.sh; break;;
+	    jackett ) /bin/zsh ./install-jackett.sh; break;;
+        transmission ) /bin/zsh ./install-transmission.sh; break;;
 	    Quit ) exit;;
     esac
 done
