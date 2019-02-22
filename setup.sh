e93913766e7de65573ea91a50a830e7cf4bb4540
atran <atran@sysaw01.neidinc.local>
2019-02-22 17:05:04 -0500

    changes

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
 echo "Iocage Jails Installation Script in FreeNAS 11.2-U2"
-echo "Which jail you want to install"
-select ans in "plexpass" "sonarr" "radarr" "jackett" "transmission" "Quit"; do
+echo "Which jail you want to install?"
+select ans in "plexpass" "sonarr" "radarr" "jackett"  "transmission" "Quit"; do
     case $ans in
         plexpass ) /bin/zsh ./plexmediaserver/plexmediaserver_plexpass.sh; break;;
         sonarr ) /bin/zsh ./sonarr.sh; break;;
