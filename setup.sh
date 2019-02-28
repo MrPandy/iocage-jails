6624025513928413f495db55abccb82494189bb0
Andy Tran <andyt315@gmail.com>
2019-02-27 20:31:52 -0500

    more fixes

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
-#!/bin/zsh
+#!/usr/local/bin/zsh
 
 echo "Iocage Jails Installation Script in FreeNAS 11.2-U2"
 echo "Which jail you want to install?"
 select ans in "plexpass" "plexserver" "sonarr" "radarr" "jackett"  "transmission" "Quit"; do
     case $ans in
-        plexpass ) /bin/zsh ./plexserver/install-plexpass.sh; break;;
-        plexserver ) /bin/zsh ./plexserver/install-plexserver.sh; break;;
-        sonarr ) /bin/zsh ./install-sonarr.sh; break;;
-        radarr ) /bin/zsh ./install-radarr.sh; break;;
-	    jackett ) /bin/zsh ./install-jackett.sh; break;;
-        transmission ) /bin/zsh ./install-transmission.sh; break;;
+        plexpass ) /usr/local/bin/zsh ./plexserver/install-plexpass.sh; break;;
+        plexserver ) /usr/local/bin/zsh ./plexserver/install-plexserver.sh; break;;
+        sonarr ) /usr/local/bin/zsh ./install-sonarr.sh; break;;
+        radarr ) /usr/local/bin/zsh ./install-radarr.sh; break;;
+	    jackett ) /usr/local/bin/zsh ./install-jackett.sh; break;;
+        transmission ) /usr/local/bin/zsh ./install-transmission.sh; break;;
 	    Quit ) exit;;
     esac
 done
