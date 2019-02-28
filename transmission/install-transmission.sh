338290e4e654720523b25b3a258581368dd9352a
Andy Tran <andyt315@gmail.com>
2019-02-27 21:30:22 -0500

    fixed paths

--- transmission/install-transmission.sh
+++ transmission/install-transmission.sh
@@ -37,17 +37,16 @@ iocage exec transmission sysrc "transmission_download_dir=/mnt/downloads/complet
 # set media permissions
 iocage exec transmission "pw useradd media -c media -u 8675309 -d /nonexistent -s /usr/bin/nologin"
 iocage exec transmission "pw groupmod media -m transmission"
-iocage exec transmission  chown -R media:media /config/transmission-home
-iocage exec transmission  chown -R media:media /mnt/downloads
+iocage exec transmission "chown -R media:media /config/transmission-home"
+iocage exec transmission "chown -R media:media /mnt/downloads"
 iocage exec transmission  sysrc "transmission_user=media"
 # disable transmission rpc-whitelist
 iocage exec transmission service transmission start
 echo "Wait 5s for transmission to create its settings"
 sleep 5
 iocage exec transmission service transmission stop
-settings="/mnt/data/iocage/jails/transmission/root/config/transmission-home/settings.json"
-sed -i 's/"rpc-whitelist": "127.0.0.1",/"rpc-whitelist": "0.0.0.0",/g' $settings
-sed -i 's/"rpc-whitelist-enabled": true,/"rpc-whitelist-enabled": false,/g' $settings
+sed -i 's/"rpc-whitelist": "127.0.0.1",/"rpc-whitelist": "0.0.0.0",/g' /mnt/data/iocage/jails/transmission/root/config/transmission-home/settings.json
+sed -i 's/"rpc-whitelist-enabled": true,/"rpc-whitelist-enabled": false,/g' /mnt/data/iocage/jails/transmission/root/config/transmission-home/settings.json
 echo "Disabled rpc-whitelist"
 sleep 3
 # start services
